07018a95ca3f8cbf03f6d15ae10e5ac847dfe642
Julie Wang <juliewang94@gmail.com>
2022-07-07 15:54:39 -0700

    modal overlay

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,16 +1,23 @@
 
 const openModalBtn = document.querySelector("#open-modal-btn")
 const closeModalBtn = document.querySelector("#close-modal-btn")
+const modal = document.querySelector("#modal")
+const overlay = document.querySelector("#overlay")
+
 
 
 openModalBtn.addEventListener("click", openModal);
 closeModalBtn.addEventListener("click", closeModal);
 
 function openModal() {
-    const modal = document.querySelector("#modal")
     modal.classList.add("open")
+    overlay.classList.add("open")
+    console.log(openModalBtn)
 }
+
 function closeModal() {
     const modal = document.querySelector("#modal")
     modal.classList.remove("open")
+    overlay.classList.remove("open")
+
 }
